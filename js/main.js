6000db2e33406bfa34c15953df67966807880253
Paul Beauchamp <paulb896@gmail.com>
2014-04-07 19:54:01 -0700

    Pass in dial attributes

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ app.directive('map', function(){
     templateUrl: 'partials/map.html',
     controller:function($scope){
       this.updateZoom = function(zoomLevel){
-        var approximateZoom = 8 + Math.round(zoomLevel / 40);
+        var approximateZoom = 9 + zoomLevel;
         $scope.map.setZoom(approximateZoom);
       }
     },
@@ -42,28 +42,25 @@ app.directive('dial', function(){
     templateUrl: 'partials/dial.html',
     require:'^map',
     link: function(scope, element, attrs, mapController) {
-      var rotationMin = 10;
-      var rotationMax = 360;
-      var dialValue = 0;
-      var dial = document.getElementById('dial');
-      Draggable.create(dial, {
+      var rotationMin = parseInt(attrs.minRotation);
+      var rotationMax = parseInt(attrs.maxRotation);
+      var dialValue = parseInt(attrs.startZoom);
+      Draggable.create(element, {
         type:'rotation',
         throwProps:true,
         dragResistance :0.05,
-        //edgeResistance:0.15,
         bounds:{minRotation:rotationMin, maxRotation:rotationMax},
         overshootThreshold:1,
-        maxDuration:0.001,
+        maxDuration:0.01,
         snap:function(endValue) {
-
           dialValue = endValue;
           return endValue;
         },
         onThrowComplete:function() {
-          mapController.updateZoom(dialValue);
+          mapController.updateZoom(Math.round(dialValue / 33));
         }
       });
-      TweenMax.to(dial, 5, {css:{rotation:320},ease:Elastic.easeOut});
+      TweenMax.to(dial, 5, {css:{rotation:dialValue},ease:Elastic.easeOut});
     }
   }
 });
