193a8578f2db20862842380196dc1140631de1af
Paul Beauchamp <paulb896@gmail.com>
2014-05-23 22:32:04 -0700

    Change map height

--- server.js
+++ server.js
@@ -10,11 +10,12 @@ config={
  * Configure express server
  */
 
-app.use(express.bodyParser())
+
 app.set('view engine', 'html');
 app.use(express.static(__dirname));
 app.set('partials', __dirname);
 app.set('views', __dirname);
+app.engine('.html', require('ejs').__express);
 
-app.listen(config.serverPort);
-console.log('Listening on port ' + config.serverPort);
+app.listen(config.serverPort, "127.0.0.1");
+console.log('Listening on 127.0.0.1,  port ' + config.serverPort);
