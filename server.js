3cfead0e9a20b6455d7c116b0f07c486dfaf945d
Paul Beauchamp <paulb896@gmail.com>
2014-09-07 19:48:20 -0700

    Remove host from listen call

--- server.js
+++ server.js
@@ -17,5 +17,5 @@ app.set('partials', __dirname);
 app.set('views', __dirname);
 app.engine('.html', require('ejs').__express);
 
-app.listen(config.serverPort, "127.0.0.1");
-console.log('Listening on 127.0.0.1,  port ' + config.serverPort);
+app.listen(config.serverPort);
+console.log('Listening on port ' + config.serverPort);
